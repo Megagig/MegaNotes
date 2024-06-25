c113f395e69fa65fc529992525c9650629993db6
Megagig <megagigsoftwaresolutions.com>
2024-06-25 20:22:21 +0100

    FInish Database and dotenv setup

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,38 @@
 import { app } from './app';
-import { initDB } from './database';
+import { closeDB, initDB } from './database';
 import { loadEnv } from './utils/load-env';
 
-const PORT = 3000;
+const { NODE_ENV } = process.env;
 
-loadEnv();
+if (NODE_ENV !== 'production') {
+  loadEnv();
+}
 
-console.log(process.env.PORT);
+const { PORT } = process.env;
 
 initDB()
   .then(() => {
-    console.log('Connected to DB');
+    console.log('Connected to DB.');
+    const server = app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+
+    process.on('SIGINT', async () => {
+      console.log('\nShutting down server...');
+      server.close(async () => {
+        await closeDB();
+        console.log('Server has been gracefully terminated.');
+        process.exit(0);
+      });
+    });
+
+    process.on('uncaughtException', async (error) => {
+      console.error('Uncaught Exception:', error);
+      await closeDB();
+      process.exit(1);
+    });
   })
-  .catch((error: unknown) => {
-    console.error('Failed to connect to DB');
+  .catch((error) => {
+    console.error('Failed to connect to DB:', error);
     process.exit(1);
   });
-
-app.listen(PORT, () => {
-  console.log('Server is running on PORT 3000');
-});
